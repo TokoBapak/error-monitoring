9ffe104863dbd8a7630b856a31d929be6baf80a8
zuramai <saugi.dev@gmail.com>
2023-01-16 21:21:09 +0700

    feat: setup unocss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,13 @@ import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
     build: {
         transpile: ['trpc-nuxt']
-    }
+    },
+    modules: [
+        '@unocss/nuxt',
+    ],
+    unocss: {
+        // presets
+        uno: true, 
+        icons: true, 
+    },
 })
