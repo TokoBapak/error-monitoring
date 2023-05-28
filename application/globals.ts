6740af6a5811fb36148d1aaff7ee8e970d3af910
Reinaldy Rafli <aldy505@proton.me>
2023-05-28 12:41:00 +0700

    fix: wrong import path for rollbarWriter

--- application/globals.ts
+++ application/globals.ts
@@ -14,18 +14,17 @@ import {UserService} from "~/application/services/User";
 import {UserClient} from "~/application/repositories/UserClient";
 import {TokenClient} from "~/application/repositories/TokenClient";
 
-const rollbarWriter = new RollbarWriter(
-    new Authentication(new SimpleMemoryCache()),
-    new ErrorLogClient(clickhouseClient),
-    new ProjectClient(postgresClient),
-);
-
+// Repository layers
 const githubClient = new GithubClient(
     process.env.GITHUB_CLIENT_ID,
     process.env.GITHUB_CLIENT_SECRET
 );
-
+const errorLogClient = new ErrorLogClient(clickhouseClient);
+const projectClient = new ProjectClient(postgresClient);
 const userRepository = new UserClient(postgresClient);
 const tokenRepository = new TokenClient(new SimpleMemoryCache(-1));
 
+// Service layers
 export const userService = new UserService(userRepository, githubClient, tokenRepository);
+export const authenticationService = new Authentication(new SimpleMemoryCache());
+export const rollbarWriter = new RollbarWriter(authenticationService, errorLogClient, projectClient);
