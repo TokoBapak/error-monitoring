cc3b21a45039c4279b83d0fa8153b5c404a95f72
Reinaldy Rafli <aldy505@proton.me>
2023-05-30 21:20:11 +0700

    Update errors/UnauthorizedError.ts
    Co-authored-by: Dicha Zelianivan Arkana <dicha.arkana03@gmail.com>

--- errors/UnauthorizedError.ts
+++ errors/UnauthorizedError.ts
@@ -1,7 +1,9 @@
-type Reason =
-    | "Not a TokoBapak member"
-    | "Not logged in"
-    | "Requires higher access";
+export const REASON = {
+    not_tokobapak_member: "Not a TokoBapak member",
+    not_logged_in: "Not logged in",
+    requires_higher_access: "Requires higher access",
+} as const;
+type Reason = typeof REASON[keyof typeof REASON];
 
 export class UnauthorizedError extends Error {
     constructor(public readonly reason: Reason) {
